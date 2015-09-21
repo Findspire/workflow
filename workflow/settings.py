20bb075bd94b726b8755898fa429e20bfafe5db3
hpusset <hpusset@gmail.com>
2015-09-21 14:32:15 +0200

    [STATIC] Fix static root dir

--- workflow/settings.py
+++ workflow/settings.py
@@ -81,7 +81,7 @@ MEDIA_URL = ''
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
