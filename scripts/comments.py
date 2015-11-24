223bca2da5bf6d1874954effb40e6db1b8e77c12
hpusset <hpusset@gmail.com>
2015-11-24 16:52:59 +0100

    remove django settings module line and set comments_count = 0

--- scripts/comments.py
+++ scripts/comments.py
@@ -8,7 +8,7 @@ import argparse
 
 
 PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "workflow.settings")
+
 sys.path.append(PATH)
 django.setup()
 
@@ -19,9 +19,9 @@ from workflow.apps.workflow.models import Item, Workflow, Comment
 def set_comments_count():
     for w in Workflow.objects.all():
         for i in w.get_items('all'):
+            Item.objects.filter(id=i.id).update(comments_count = 0)
             for c in Comment.objects.filter(item=i):
                 Item.objects.filter(id=c.item.id).update(comments_count = F('comments_count') + 1)
-
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Set comments_count field of items')
